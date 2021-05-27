5637014c815553ff0d0eb359368d9f706446a7c5
tannhn0420 <tannhn0420@gmail.com>
2021-05-27 23:49:18 +0700

    My Project Setup Completed Successfully

---
+++ shoppingmallbackend/src/main/java/com/nattahn/shoppingmallbackend/App.java
@@ -0,0 +1,13 @@
+package com.nattahn.shoppingmallbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
